09fe1a96570e333568d5d122e6f480d09058ab51
NickBowley <116365231+NickBowley@users.noreply.github.com>
2023-01-11 10:33:19 +0000

    Add files via upload

---
+++ Fly.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Fly here.
+ * 
+ * @author Nick Bowley 
+ * @version 
+ */
+public class Fly extends Actor
+{
+    /**
+     * Act - do whatever the Fly wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    
+{
+  
+               }
+          }
+
+
+
+  
