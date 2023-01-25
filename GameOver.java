436dc686d156bf4518a638cd073d397d8f5f558e
NickBowley <116365231+NickBowley@users.noreply.github.com>
2023-01-25 10:01:49 +0000

    Create GameOver.java

---
+++ GameOver.java
@@ -0,0 +1,13 @@
+import greenfoot.*;
+
+public class GameOver extends Actor
+{
+    Color[] colors = { Color.ORANGE, Color.RED };
+    int colorNum = 0;
+    int counter = 1;
+
+   
+    
+    
+    
+    }
