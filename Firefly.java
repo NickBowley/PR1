827b1db4a4ff80e125f8676eca8b59fed750883c
NickBowley <116365231+NickBowley@users.noreply.github.com>
2023-01-25 10:01:11 +0000

    Create Firefly.java

---
+++ Firefly.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Firefly here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Firefly extends Fly
+{
+    /**
+     * Act - do whatever the Firefly wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.{ 
+        move(4);
+    if (Greenfoot.getRandomNumber(100) <10)
+    
+    {
+        turn(Greenfoot.getRandomNumber(90) - 45);
+    }
+    
+    if (getX() <= 5 || getX() >= getWorld().getWidth() - 5)
+    
+    {
+        turn(180);
+    }
+    
+    if (getY() <= 5 || getY() >= getWorld().getHeight() - 5)    
+    {
+        turn(180);
+    }
+    
+  
+               }
+          }
