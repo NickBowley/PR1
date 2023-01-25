82b0980fd5b0642d2caef9d60203da78501e4fee
NickBowley <116365231+NickBowley@users.noreply.github.com>
2023-01-25 10:02:34 +0000

    Update Lizard.java

--- Lizard.java
+++ Lizard.java
@@ -8,56 +8,65 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Lizard extends Actor
 {   
+    
+    Color[] colors = { Color.ORANGE, Color.RED };
+    int colorNum = 0;
+    int counter = 1;
+    
+    
+    
         
     /**
      * Act - do whatever the Snake wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     
+       
+    
+    
     
     public void act() 
     {
         goTo();
-        
         checkPlayerCollision();
         
-        
-       
-         
-        
+    
     } 
 
 
      public void goTo()
     {
         Player player = (Player)getWorld().getObjects(Player.class).get(0);
-    int PlayerX = player.getX();
-    int PlayerY = player.getY();
-        
-    turnTowards(PlayerX,PlayerY);
+        int PlayerX = player.getX();
+        int PlayerY = player.getY();
+        turnTowards(PlayerX,PlayerY);
         move(2);
+        
+        
     }
+    
     public void checkPlayerCollision()
     {
-        if(isTouching(Player.class))
+    
+          if(isTouching(Player.class))
         {
-            
-            removeTouching(Player.class);
-            Greenfoot.stop();
-            
+        removeTouching(Player.class);
+        updateImage();
             
         }
-        
-        
+            
+            
+    }
+
+    
+
+     public void updateImage()
+    {
+        setImage(new GreenfootImage("GAME\nOVER", 96, colors [colorNum], new Color(0, 0, 0, 0)));
+        new Color(0, 0, 0, 0);
     }
         
         
-        
-
-    
-        
-        
- 
     
 }
 
