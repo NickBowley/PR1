1db243031eb11852ab78280fb5b411210198ac6d
NickBowley <116365231+NickBowley@users.noreply.github.com>
2023-01-26 14:18:49 +0000

    Update Player.java
    Added a way to win the game

--- Player.java
+++ Player.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * the Frog class is the player's character. They can move and eat flies.
+ * the Player class is the player's character. They can move and eat flies.
  * 
  * @author Nick Bowley 
  * @author Piers Watson
@@ -11,14 +11,19 @@ public class Player extends Actor
 {
     int fireFliesEaten = 0;
     int flysEaten = 0;
-    int turnRate = 4;
+    int turnRate = 3;
     int turnRateEdge = 20;
     int jumpDistance = 100;
     int jumpDelay = 0;
     int energy = 0;
     int maxEnergy = 5;
     
-    public void checkCollision()
+    
+
+    
+    
+    
+        public void checkCollision()
     {
         if(isTouching(Fly.class))
         {
@@ -27,30 +32,33 @@ public class Player extends Actor
         
             flysEaten +=1;
         
-            MyWorld mw = (MyWorld)getWorld();
-            mw.showText("Score is: " + flysEaten, 70,20);
+        MyWorld mw = (MyWorld)getWorld();
+        mw.increaseScore(+1);
+            
+    }  
+        
+        
+        if(isTouching(Firefly.class))
+        
+        {
+            removeTouching(Firefly.class);
+        
+        MyWorld mw = (MyWorld)getWorld();
+        fireFliesEaten +=2;
+        mw.increaseScore(5);
             
         }
+        
+        
+        
+        
             
-                
-            
-        
-       
-        
-        
-        
-    }
-    
-    public void checkFireflyCollision()
-    
-    {
-        if(isTouching(Firefly.class))
-        removeTouching(Firefly.class);
-        flysEaten +=1;
     }
     
     public void act()
     {
+        checkCollision();
+        
         //this if condition allows the jump delay to tick down.
         if(jumpDelay > 0)
         {
@@ -65,7 +73,7 @@ public class Player extends Actor
         }
         
         //this one is supposed to make you turn around at the edge, 
-        //but we'll probably have to adapt that to killing the frog.
+        //but we'll probably have to adapt that to killing the Frog
         if (isAtEdge())
         {
             turn(turnRateEdge);
@@ -82,11 +90,11 @@ public class Player extends Actor
             turn(turnRate);
         }
         
-        checkCollision();
+
+    }
         
-        checkFireflyCollision();
-    }        
-}   
+   
+    }      
 
                     
                  
